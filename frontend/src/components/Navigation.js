90652d61f91c1423b8980d9da5e4ef49cba2772e
JiachengBai <133056200+JiachengBai@users.noreply.github.com>
2025-06-04 20:54:20 -0700

    Updated Nav Bar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -53,7 +53,7 @@ const Navigation = () => {
                 ) : (
                     <>
                         <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
+                        <Link to="/register">Sign Up</Link>
                     </>
                 )}
             </div>
